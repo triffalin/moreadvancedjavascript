610e55bd1cef9ebed5ebab50444185a0c817d049
Alin Triff <triffalin@gmail.com>
2023-09-23 19:20:31 +0300

    examples of functions and default parameters

--- functions.js
+++ functions.js
@@ -1,5 +1,13 @@
-function greetUser(userName) {
+function greetUser(userName = 'user') {
 	console.log('Hi' + userName + '!');
 }
 
-greetUser('Alin');
+greetUser('Alin'); // Hi Alin!
+greetUser(); // Hi undefined without value in parameter, with value => Hi user
+
+function greetUserr(greetingPrefix, userName = 'user') {
+	console.log(greetingPrefix + '' + userName + '!');
+}
+
+greetUserr('Hi', 'Alin'); // Hi Alin!
+greetUserr('Hello'); // Hello user!
