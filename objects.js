5e23473372031713439c69b8e78409bf89a48cac
Alin Triff <triffalin@gmail.com>
2023-09-25 17:00:30 +0300

    added methods and called on objects

--- objects.js
+++ objects.js
@@ -20,9 +20,18 @@ class Job {
 		this.location = place;
 		this.salary = salary;
 	}
+	// 9a. classes and methods(this)
+	describe() {
+		console.log(
+			`I'm a ${this.title}, I work in ${this.location} and I earn ${this.salary}.`
+		);
+	}
 }
 
 const developer = new Job('Developer', 'New York', 50000);
 const cook = new Job('Cook', 'Munich', 35000);
 console.log(developer); // => Job { title: 'Developer', location: 'New York', salary: 50000 }
 console.log(cook); // => Job { title: 'Cook', location: 'Munich', salary: 35000 }
+// 9b. call classes and methods(this)
+developer.describe();
+cook.describe();
