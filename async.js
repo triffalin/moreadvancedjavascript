13f692f204eb009d14c565f3d28f0c4d0e395882
Alin Triff <triffalin@gmail.com>
2023-09-25 17:45:09 +0300

    added the promises

--- async.js
+++ async.js
@@ -1,14 +1,23 @@
 // 11. asyncronous code and callback functions
 
-const fs = require('fs');
+const fs = require('fs/promises');
 
 function readFile() {
 	let fileData;
 
-	fileData = fs.readFile('data.txt', function (error, fileData) {
-		console.log('File parsing done!');
-		console.log(fileData.toString());
-	});
+	// fileData = fs.readFile('data.txt', function (error, fileData) {
+	// 	console.log('File parsing done!');
+	// 	console.log(fileData.toString());
+	// 	// 12. start another async task that sends the data to a database (promises)
+	// });
+
+	fs.readFile('data.txt')
+		.then(function (fileData) {
+			console.log('File parsing done!');
+			console.log(fileData.toString());
+			//return anotherAsyncOperation;
+		})
+		.then(function () {});
 
 	console.log('Hi there!');
 }
