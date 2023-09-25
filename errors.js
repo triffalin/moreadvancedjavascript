d750326a12e396358efd14b26f79ad3c28fc879a
Alin Triff <triffalin@gmail.com>
2023-09-25 16:08:57 +0300

    create custom error handling with try and catch

---
+++ errors.js
@@ -0,0 +1,13 @@
+// 6. Custom error handling with try/catch
+
+const fs = require('fs');
+
+function readFile() {
+	try {
+		const fileData = fs.readFileSync('data.json');
+	} catch {
+		console.log('An error occurred!');
+	}
+
+	console.log('Hi there!');
+}
