7ae8dd8d0fc11df96f81f208bd8a88e7b9d55321
Alin Triff <triffalin@gmail.com>
2023-09-25 09:40:55 +0300

    introduced the rest parameters and the spread operator

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+// 1. functions & default parameters
 function greetUser(userName = 'user') {
 	console.log('Hi' + userName + '!');
 }
@@ -11,3 +12,54 @@ function greetUserr(greetingPrefix, userName = 'user') {
 
 greetUserr('Hi', 'Alin'); // Hi Alin!
 greetUserr('Hello'); // Hello user!
+
+// 2. rest parameters & the spread operator
+function sumUp(num1, num2, num3) {
+	return num1 + num2 + num3;
+}
+
+console.log(sumUp(1, 2)); // 1 + 2 + undefined = NaN (not a number)
+
+function sumUpT(num1, num2, num3 = 0) {
+	return num1 + num2 + num3;
+}
+
+console.log(sumUpT(1, 2)); // 1 + 2 + 0 = 3
+
+function sumUpArray(numbers) {
+	let result = 0;
+
+	for (const number of numbers) {
+		result += number; // result = result + number
+	}
+
+	return result;
+}
+
+console.log(sumUpArray([1, 5, 10, 11])); // result = 27
+
+function sumUpRest(...numbers) {
+	let result = 0;
+
+	for (const number of numbers) {
+		result += number; // result = result + number
+	}
+
+	return result;
+}
+
+console.log(sumUpRest(1, 5, 10, 11, 20, 31)); // result = 78
+
+function sumUpSpread(...numbers) {
+	let result = 0;
+
+	for (const number of numbers) {
+		result += number; // result = result + number
+	}
+
+	return result;
+}
+
+const inputNumbers = [1, 5, 10, 11, 20, 31];
+
+console.log(sumUpSpread(...inputNumbers)); // result = 78
