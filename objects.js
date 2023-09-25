6e548b831acb3edb6ed960c9817ce5b8ebc4e625
Alin Triff <triffalin@gmail.com>
2023-09-25 16:54:27 +0300

    introduced the classes as objects blueprints

---
+++ objects.js
@@ -0,0 +1,28 @@
+// 8. introducing classes as objects blueprints
+
+// const job = {
+// 	title: 'Developer',
+// 	location: 'New York',
+// 	salary: 50000,
+// };
+
+// console.log(new Date().toISOString());
+
+// const job2 = {
+// 	title: 'Cook',
+// 	location: 'Munich',
+// 	salary: 35000,
+// };
+
+class Job {
+	constructor(jobTitle, place, salary) {
+		this.title = jobTitle;
+		this.location = place;
+		this.salary = salary;
+	}
+}
+
+const developer = new Job('Developer', 'New York', 50000);
+const cook = new Job('Cook', 'Munich', 35000);
+console.log(developer); // => Job { title: 'Developer', location: 'New York', salary: 50000 }
+console.log(cook); // => Job { title: 'Cook', location: 'Munich', salary: 35000 }
